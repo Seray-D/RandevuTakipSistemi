4d79beeb207ed327de557e22a02b37d51ea7cd4a
Seray-D <seray.dagdelen@tedu.edu.tr>
2026-07-13 17:02:33 +0300

    syntax hatası düzeltildi

--- Main/Data/AppDbContext.cs
+++ Main/Data/AppDbContext.cs
@@ -5,12 +5,12 @@ namespace Main.Data
 {
     public class AppDbContext : DbContext
     {
-        
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        
         public DbSet<Appointment> Appointments { get; set; }
-        public Dbgit add.Set<Category> Categories { get; set; }
+        
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Note> Notes { get; set; }
     }
 }
